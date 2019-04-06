619142e2b00bdefed3e416e2e53de057eaef466c
MacBook Pro <macbookpro@MacBook-Pro-4.local>
2019-04-07 01:56:29 +0200

    Filter function

--- index.php
+++ index.php
@@ -2,9 +2,7 @@
 <head>
     <title> Friend Book </title>
     <header style='text-align: center;'> <h1> Friend Book <h1></header>
-    
-</head>
-
+    </head>
 <body>
 
 <form action="index.php" method="post">
@@ -13,23 +11,51 @@ Name: <input type="text" name="name">
 </form><br><br><br>
 
 <?php 
-
 $filename = 'friends.txt';
 
-$file = fopen( $filename, "a" );
-fwrite( $file, PHP_EOL.$_POST["name"] );
+$file = fopen( $filename, "a+" );
 
-$file = fopen( $filename, "r" );
+if (isset($_POST["name"]) && $_POST["name"]!="")
+    fwrite( $file, PHP_EOL.$_POST["name"] );
+
+rewind($file);
+
+echo "<ol>";
 while (!feof($file)) {
-
-   echo "<li>".fgets($file)."</li>"."<br>";
+    echo "<li>".fgets($file)."</li>"."<br>";
 } 
+echo "</ol>";
 
-
+rewind($file);
 ?>
+
+<form action='index.php' method='post'>Filter: <input type='text' name='nameFilter' value="<?=$nameFilter?>"><input type='submit'></form><br>
+
+<?php
+if (isset($_POST["nameFilter"])){
+    $tab = array();
+
+    while (!feof($file)) {
+        $file2=fgets($file);
+        $RESULT=stristr($file2,$_POST["nameFilter"]);
+        if ($RESULT==$file2){
+            array_push($tab,$RESULT);
+        }
+    } 
+    echo "<ol>";
+    foreach($tab as $friend){
+        echo "<li>".$friend."</li>";
+
+    }
+    echo "</ol>";
+}
+
+fclose($file);
+?>
+
+<br><br>
 
 
 <h1 style='text-align: center;' >  Footer </h1> 
-
 </body>
 </html>
